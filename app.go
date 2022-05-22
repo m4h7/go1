d2c399fe7443e30b06fb51fccc15aea0ef7584d8
mh <mh@github>
2022-05-22 15:56:52 +0100

    app.go

---
+++ app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", handlerFunc)
+    log.Fatal(http.ListenAndServe("0.0.0.0:9090", nil))
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Ping from %s", r.RemoteAddr)
+    fmt.Fprintln(w, "Hello World")
+}
